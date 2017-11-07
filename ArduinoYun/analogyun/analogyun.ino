a51baa012452a1fb17cb8e33e8a1dd43cb0a133d
Carl0sHdz <carlitos.hernandez203@gmail.com>
2017-11-07 14:33:51 -0600

    Aca se contienen ejemplos de como manipular los pines de un arduino Yun

---
+++ ArduinoYun/analogyun/analogyun.ino
@@ -0,0 +1,43 @@
+#include<Bridge.h>
+#include<YunServer.h>
+#include<YunClient.h>
+
+YunServer server;
+
+int led = 2;
+int lectura = 0;
+int url = 0;
+float vol = 0.0;
+int decimal = 0;
+
+void setup() {
+  pinMode(led,OUTPUT);
+  Bridge.begin();
+  server.listenOnLocalhost();
+  server.begin();
+}
+
+void loop() {
+  YunClient client = server.accept();
+  lectura = analogRead(A5);
+  vol = map(lectura,0,1023,0,500);
+  decimal = int(vol);
+  if(client)                                      //http://arduino.local/arduino/
+  {
+    String comando = client.readStringUntil('/');
+    if(comando == "analogYun")                    //http://arduino.local/arduino/analogYun
+    {
+      url = client.parseInt();
+      if(url == 5)                                //http://arduino.local/arduino/analogYun/5
+      {
+        client.print("el voltaje es: ");
+        client.print(int(vol/100));
+        client.print(".");
+        client.print(decimal%100);
+        client.print("V");
+      }
+    }
+    client.stop();
+  }
+  delay(50);
+}
